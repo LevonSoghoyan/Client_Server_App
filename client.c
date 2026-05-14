21ef7378db5d5864d5de4df621f39014c3a58ea4
Levon Soghoyan <levon.soghoyan@taurotech.com>
2026-05-14 21:33:25 +0400

    Added Clients,connect,shell,client status command implementation
    Added multyple client functionality

--- client.c
+++ client.c
@@ -1,30 +1,35 @@
 /*INCLUDES*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <ctype.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+
 /*MACROS*/
 #define PORT 8080
 #define BUFF_SIZE 2048
+
 /*Global variables*/
 char buf[BUFF_SIZE];
 int client_fd, status;
 struct sockaddr_in server_addr;
 socklen_t addr_len = sizeof(server_addr);
-void Connect(char* IP)
-{
-    if (inet_pton(AF_INET, IP, &server_addr.sin_addr) <= 0) {
 
-        printf("ip_addr   \n");
+void Connect(char* IP)
+    
+{
+    if (inet_pton(AF_INET, IP, &server_addr.sin_addr) < 0) {
+        printf("Error: %s \n",strerror(errno));
     }
+
     /*Connecting client whit server*/
     if ((status = connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr))) < 0) {
-
-        printf("connection   \n");
+        printf("Error: %s\n",strerror(errno));
     }
+
     return;
 }
 
@@ -38,8 +43,14 @@ void Shell(char* command)
 
     send(client_fd, command, BUFF_SIZE, 0);
     memset(buf, 0, BUFF_SIZE);
+    
     recv(client_fd, buf, BUFF_SIZE, 0);
-    printf("%s", buf);
+    while(strcmp(buf,"NULL") != 0)
+    {
+        printf("%s", buf);
+        recv(client_fd, buf, BUFF_SIZE, 0);
+    }
+
     return;
 }
 
@@ -47,6 +58,7 @@ void Status()
 {
     char ip[INET_ADDRSTRLEN];
     struct sockaddr_in addr;
+
     if (getpeername(client_fd, (struct sockaddr *)&addr, &addr_len) == 0)
         inet_ntop(AF_INET, &addr.sin_addr, ip, INET_ADDRSTRLEN);
     if (client_fd) {
@@ -59,17 +71,19 @@ void Status()
 int main(int argc, char* argv[])
 {
     printf("\nCLIENT CLI\n");
-    printf("USE help for help massage\n");
+
     /*Creating Socket*/
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("socet crt\n");
+        printf("Error: %s \n",strerror(errno));
     }
+
     /*Adding server configurations*/
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
 
     while(1) {
-        printf("Client >");
+        printf("Client> ");
+        
         char buf[BUFF_SIZE];
         fgets(buf, BUFF_SIZE, stdin);
 
@@ -81,6 +95,7 @@ int main(int argc, char* argv[])
         char *token;
         token = strtok(buf, " ");
         token[strcspn(token,"\n")] = '\0';
+
         if (strcmp(token, "connect") == 0) {
             Connect((buf+strlen(token) + 1));
         } else if (strcmp(token, "shell") == 0) {
@@ -92,10 +107,10 @@ int main(int argc, char* argv[])
         } else {
             printf("Usage:\n");
             printf("COMMAND                     DESCRIPTION\n");
-            printf("connect  <IP>:              connect to server by <IP>\n");    
-            printf("shell <command>:            run <command> in server terminal\n");    
-            printf("disconnect:                 disconnect client \n");    
-            printf("status:                     show connection status\n");    
+            printf("connect  <IP>               connect to server by <IP>\n");
+            printf("shell <command>             run <command> in server terminal\n");
+            printf("disconnect                  disconnect client \n");
+            printf("status                      show connection status\n");
         }
     }
 }
--- server.c
+++ server.c
@@ -1,5 +1,10 @@
 /*INCLUDES*/
+#include <time.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -7,42 +12,50 @@
 #include <ctype.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+
 /*MACROS*/
 #define PORT 8080
 #define BUFF_SIZE 2048
+#define CLIENTS_COUNT 10
+
+#define PROMPT "Server> "
+
 /*Global variables*/
 char Server_Command[BUFF_SIZE];
-int server_fd, status;
-int client_fd;
+char PS[BUFF_SIZE];
 struct sockaddr_in address;
 socklen_t addrlen = sizeof(address);
+int server_fd, status;
 int opt = 1;
-/********************************
- *@breef
+int client_fd;
 
+/*Struct*/
+typedef struct Clients {
+    int socket;
+    char time[30];
+} Tclients;
 
- ******************************/
-int Server_Up()
+Tclients *Clients;
+
+void Server_Up()
 {
+    printf("Server is running\n");
+}
 
-    if (listen(server_fd, 3) < 0) {
+void Server_Down()
+{
+    exit(0);
+}
 
-        printf("listen\n");
+void clients()
+{
+    for(int i = 0; i < CLIENTS_COUNT; i++) {
+        if (Clients[i].socket != 0) {
+            printf("Socket %d | Crated in %s", Clients[i].socket, Clients[i].time);
+        }
     }
-
-    client_fd = accept(server_fd, (struct sockaddr*)&address,&addrlen);
 }
 
-int Server_Down()
-{
-    for(int i = 0; i < 10 ; i++)
-        close(client_fd);
-    close(server_fd);
-}
-
-int *clients()
-{
-}
 void Status()
 {
     char ip[INET_ADDRSTRLEN];
@@ -52,9 +65,10 @@ void Status()
         printf("The server IP is %s \n", ip);
     }
 }
-int ps()
+
+void ps()
 {
-    system("ps");
+    printf("%s\n", PS);
 }
 
 void history()
@@ -66,74 +80,42 @@ void Client_Command()
 {
     memset(Server_Command, 0, BUFF_SIZE);
     recv(client_fd , Server_Command, BUFF_SIZE, 0);
+
     if (strcmp(Server_Command, "shell") == 0) {
 
         char buffer[BUFF_SIZE];
-        char output[BUFF_SIZE];
-        memset(output, 0, BUFF_SIZE);
         memset(buffer, 0, BUFF_SIZE);
-        char* pcmd  = Server_Command + 6;
+
+        char* pcmd = Server_Command + 6;
         FILE* pf;
         pf = popen(pcmd, "r");
+
         while(fgets(buffer, sizeof(buffer), pf) != NULL) {
-            strcat(output,buffer);
+            send(client_fd, buffer, BUFF_SIZE, 0);
+            memset(buffer, 0, BUFF_SIZE);
         }
-        send(client_fd, output, BUFF_SIZE, 0);
+        strcpy(buffer, "NULL");
+        send(client_fd, buffer, BUFF_SIZE, 0);
         pclose(pf);
     }
 }
-int main(int argc, char* argv[])
+
+char* IP;
+void Server_CLI()
 {
-    printf("\nSERVER CLI\n");
-    printf("USE help for help message\n");
-    /*Creating server socket*/
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("socket\n");
-    }
-    /*Adding server config*/
-    if (setsockopt(server_fd, SOL_SOCKET,
-                SO_REUSEADDR | SO_REUSEPORT, &opt,
-                sizeof(opt))) {
-        ////
-    }    
-    char* IP;
-    if (argc > 1) {
-        IP = argv[1];
-    } else {
-        IP = "127.0.0.1";
-    }
-    
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(IP);
-    address.sin_port = htons(PORT);
-
-    /* Making server socket to listening mode*/
-    if (bind(server_fd, (struct sockaddr*)&address,sizeof(address)) < 0) {
-
-        printf("bind\n");
-    }
-    
-    Server_Up();
-    pid_t pid = fork();
-    int UP = 0;
     char Server_Command[BUFF_SIZE];
-    
+
     while(1) {
 
-        while (pid == 0) {
-            Client_Command();
-            close(server_fd);
-        }
-        printf("Server>");
-        close(client_fd);
+        printf(PROMPT);
+
         fgets(Server_Command, BUFF_SIZE, stdin);
-        Server_Command[strcspn(Server_Command,"\n")] = '\0';
+        Server_Command[strcspn(Server_Command, "\n")] = '\0';
 
         if (strlen(Server_Command) <= 1)
             continue;
 
         if (strcmp(Server_Command, "up") == 0) {
-            UP = 1;
             Server_Up();
         } else if (strcmp(Server_Command, "down") == 0) {
             Server_Down();
@@ -148,12 +130,105 @@ int main(int argc, char* argv[])
         } else {
             printf("Usage:\n");
             printf("COMMAND                     DESCRIPTION\n");
-            printf("up:                         Start server\n");    
-            printf("down:                       Stop server\n");    
-            printf("client:                     Show connection list\n");    
-            printf("status:                     Show connection status\n");    
-            printf("ps:                         Show process list\n");    
-            printf("history:                    History\n");    
+            printf("up                         Start server\n"); 
+            printf("down                       Stop server\n");
+            printf("clients                    Show connection list\n");
+            printf("status                     Show connection status\n");
+            printf("ps                         Show process list\n");
+            printf("history                    History\n");
+        }
+    }
+}
+
+void Add_Client(int new_client)
+{
+    for(int i = 0; i < CLIENTS_COUNT; i++) {
+
+        if (Clients[i].socket == 0) {
+
+            Clients[i].socket = new_client;
+            time_t Ctime;
+            time(&Ctime);
+            strncpy(Clients[i].time, ctime(&Ctime), sizeof(Clients[i].time));
+            break;
+        }
+    }
+}
+int main(int argc, char* argv[])
+{
+
+    Clients = mmap(NULL, sizeof(Tclients) * CLIENTS_COUNT, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    
+    memset(Clients, 0, sizeof(Tclients) * CLIENTS_COUNT);
+
+    if (argc > 1) {
+        IP = argv[1];
+    } else {
+        IP = "127.0.0.1";
+    }
+
+    pid_t pid = fork();
+
+    if (pid == 0 )
+    {
+        Server_CLI();
+        exit(0);
+    }
+
+    printf("\nSERVER CLI\n");
+
+    /*Creating server socket*/
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error: %s \n", strerror(errno));
+    }
+
+    /*Adding server config*/
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(IP);
+    address.sin_port = htons(PORT);
+
+    /* Making server socket to listening mode*/
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        printf("Error: %s \n", strerror(errno));
+    }
+
+    if (listen(server_fd, 3) < 0) {
+
+        printf("Error: %s \n", strerror(errno));
+    }
+
+    while (1) {
+
+        struct sockaddr_in client_address;
+        socklen_t addrlen = sizeof(client_address);
+
+        client_fd = accept(server_fd, (struct sockaddr*)&client_address, &addrlen);
+
+        printf("-------------");
+        printf("\nClient connected. Port is %u\n", ntohs(client_address.sin_port));
+        printf("-------------");
+
+        Add_Client(client_fd);
+        pid_t new_pid = fork();
+
+        if(new_pid == 0) {
+
+            close(server_fd);
+
+            while (waitpid(-1, NULL, WNOHANG)) {
+                Client_Command();
+            }
+
+            exit(0);
+
+        } else if (new_pid > 0) {
+            continue;
+        } else {
+            printf("Error: %s \n", strerror(errno));
         }
     }
 }
